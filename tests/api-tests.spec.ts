79644029c8876705e23bbb531472edd22a2452bc
Jyothika-S <jyothi0351@gmail.com>
2024-03-04 19:12:25 +0530

    assertions and response data parsing

--- tests/api-tests.spec.ts
+++ tests/api-tests.spec.ts
@@ -3,12 +3,13 @@ import { expect, test } from '@playwright/test';
 //GET API Request
 test('API Get request', async({request}) =>{
     const response = await request.get('https://reqres.in/api/users/2')
+    //response code assertion - validate status code
     expect(response.status()).toBe(200);
 
     const resdata = await response.text();
     console.log("resdata",resdata);
     expect(resdata).toContain('Janet');
-
+    //parsing json response data
     console.log("response data - ", await response.json());
 
 })
@@ -21,11 +22,13 @@ test('API POST request', async({request}) =>{
             "email": "jyothi@gmail.com"
         }
     })
+    //response code assertion - validate status code
     expect(response.status()).toBe(201);
 
     const resdata = await response.text();
     expect(resdata).toContain('jyothi');
-
+    
+    //parsing json response data
     console.log("response data - ", await response.json());
 })
 
@@ -37,17 +40,20 @@ test('API PUT request', async({request}) =>{
             "email": "jyothi@gmail.com"
         }
     })
+    //response code assertion - validate status code
     expect(response.status()).toBe(200);
 
     const resdata = await response.text();
     expect(resdata).toContain('jyothi');
-
+    //parsing json response data
     console.log("response data - ", await response.json());
 })
 
 //DELETE API Request
 test('API Delete request', async({request}) =>{
     const response = await request.delete('https://reqres.in/api/users/2')
+    //response code assertion - validate status code
+    // expect (response.ok()).toBeTruthy();
     expect(response.status()).toBe(204);
 
 })
